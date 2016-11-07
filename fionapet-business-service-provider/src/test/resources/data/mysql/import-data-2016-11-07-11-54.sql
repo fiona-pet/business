08f6f775f0720edd56b49d22d68279c2c66e54f8
tom <tom@daoshenggroup.com>
2016-11-07 15:54:11 +0800

    update product data

---
+++ fionapet-business-service-provider/src/test/resources/data/mysql/import-data-2016-11-07-11-54.sql
@@ -0,0 +1,4 @@
+-- --t_user_dict_detail-- --
+INSERT into t_user_dict_detail (id, dict_type_id,dict_detail_code,value_name_cn,value_name_en,comments,create_user_id,update_user_id) VALUES (uuid(), 'f5ca1dcb-41e4-4553-b41e-f3eeda6d56f7', 'DM0000000078', '0.1ml/kg', NULL, NULL, '34ff5c2e-6625-4d6d-ad39-d1745755b3a8','34ff5c2e-6625-4d6d-ad39-d1745755b3a8');
+INSERT into t_user_dict_detail (id, dict_type_id,dict_detail_code,value_name_cn,value_name_en,comments,create_user_id,update_user_id) VALUES (uuid(), 'f5ca1dcb-41e4-4553-b41e-f3eeda6d5610', 'DM00094', '次/眼', NULL, NULL, '34ff5c2e-6625-4d6d-ad39-d1745755b3a8','34ff5c2e-6625-4d6d-ad39-d1745755b3a8');
+-- -t_user_dict_detail-- --
