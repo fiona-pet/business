376e6b06f934413b8038c7137ed3be6a5635bbcb
tom <tom@daoshenggroup.com>
2017-03-12 09:34:32 +0800

    住院 充值

--- fionapet-business-service-provider/src/main/java/com/fionapet/business/service/PrepayMoneyServiceImpl.java
+++ fionapet-business-service-provider/src/main/java/com/fionapet/business/service/PrepayMoneyServiceImpl.java
@@ -1,5 +1,6 @@
 package com.fionapet.business.service;
 
+import com.fionapet.business.entity.InHospitalRecord;
 import com.fionapet.business.entity.PrepayMoney;
 import org.dubbo.x.repository.DaoBase;
 import org.dubbo.x.service.CURDServiceBase;
@@ -13,9 +14,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class PrepayMoneyServiceImpl extends CURDServiceBase<PrepayMoney> implements PrepayMoneyService {
     @Autowired
     private PrepayMoneyDao prepayMoneyDao;
+    @Autowired
+    private InHospitalRecordService inHospitalRecordService;
 
     @Override
     public DaoBase<PrepayMoney> getDao() {
         return prepayMoneyDao;
+    }
+
+    @Override
+    public PrepayMoney createOrUpdte(PrepayMoney entity) {
+        String relationId = entity.getRelationId();
+
+        InHospitalRecord inHospitalRecord = inHospitalRecordService.detail(relationId);
+
+        if (null != inHospitalRecord){
+            inHospitalRecord.setInputMoney(inHospitalRecord.getInputMoney() + entity.getInputMoney());
+            inHospitalRecordService.createOrUpdte(inHospitalRecord);
+        }
+
+        return super.createOrUpdte(entity);
     }
 }
